214f767c219903e2c01b85f540eb075476f85bf1
Frank Nkuyahaga <frank@Franks-MacBook-Pro.local>
2018-08-07 12:19:33 +0300

    Add support for Bahasa Indonesia

--- app/scripts/models/Langs.js
+++ app/scripts/models/Langs.js
@@ -1,6 +1,7 @@
 (function(module) {
     mifosX.models = _.extend(module, {
         Langs: [
+			{ "name": "Bahasa Indonesia", "code": "id"},
             { "name": "English", "code": "en" },
             { "name": "Français", "code": "fr" },
             { "name": "Español", "code": "es" },
