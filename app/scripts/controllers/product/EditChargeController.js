985c334a18670cec6fb20a994f5387e11f4893d9
Frank Nkuyahaga <frank@Franks-MacBook-Pro.local>
2019-04-11 15:07:06 +0300

    Revert "Implement overdue penalties with ranges"
    This reverts commit 5f7eb9bb41af0951627a570920393c78183b2ea4.

--- app/scripts/controllers/product/EditChargeController.js
+++ app/scripts/controllers/product/EditChargeController.js
@@ -1,188 +1,162 @@
 (function (module) {
-	mifosX.controllers = _.extend(module, {
-		EditChargeController: function (scope, resourceFactory, location, routeParams, dateFilter) {
-			scope.template = [];
-			scope.showdatefield = false;
-			scope.repeatEvery = false;
-			scope.first = {};
-			scope.flag = false;
-			scope.showPenalty = true;
+    mifosX.controllers = _.extend(module, {
+        EditChargeController: function (scope, resourceFactory, location, routeParams, dateFilter) {
+            scope.template = [];
+            scope.showdatefield = false;
+            scope.repeatEvery = false;
+            scope.first = {};
+            scope.flag = false;
+	        scope.showPenalty = true ;
 
-			resourceFactory.chargeResource.getCharge({chargeId: routeParams.id, template: true}, function (data) {
-				scope.template = data;
-				scope.incomeAccountOptions = data.incomeOrLiabilityAccountOptions.incomeAccountOptions || [];
-				scope.liabilityAccountOptions = data.incomeOrLiabilityAccountOptions.liabilityAccountOptions || [];
-				scope.incomeAndLiabilityAccountOptions = scope.incomeAccountOptions.concat(scope.liabilityAccountOptions);
+            resourceFactory.chargeResource.getCharge({chargeId: routeParams.id, template: true}, function (data) {
+                scope.template = data;
+                scope.incomeAccountOptions = data.incomeOrLiabilityAccountOptions.incomeAccountOptions || [];
+                scope.liabilityAccountOptions = data.incomeOrLiabilityAccountOptions.liabilityAccountOptions || [];
+                scope.incomeAndLiabilityAccountOptions = scope.incomeAccountOptions.concat(scope.liabilityAccountOptions);
 
-				if (data.chargeAppliesTo.value === "Loan") {
-					scope.chargeTimeTypeOptions = data.loanChargeTimeTypeOptions;
-					scope.template.chargeCalculationTypeOptions = scope.template.loanChargeCalculationTypeOptions;
-					scope.flag = false;
-					scope.showFrequencyOptions = true;
-				} else if (data.chargeAppliesTo.value === "Savings") {
-					scope.chargeTimeTypeOptions = data.savingsChargeTimeTypeOptions;
-					scope.template.chargeCalculationTypeOptions = scope.template.savingsChargeCalculationTypeOptions;
-					scope.flag = true;
-					scope.showFrequencyOptions = false;
-				} else if (data.chargeAppliesTo.value === 'Shares') {
-					scope.showChargePaymentByField = false;
-					scope.chargeCalculationTypeOptions = scope.template.shareChargeCalculationTypeOptions;
-					scope.chargeTimeTypeOptions = scope.template.shareChargeTimeTypeOptions;
-					scope.addfeefrequency = false;
-					scope.showGLAccount = false;
-					scope.showPenalty = false;
-					scope.flag = true;
-				} else {
-					scope.flag = true;
-					scope.template.chargeCalculationTypeOptions = data.clientChargeCalculationTypeOptions;
-					scope.chargeTimeTypeOptions = scope.template.clientChargeTimeTypeOptions;
-					scope.showFrequencyOptions = false;
-					scope.showGLAccount = true;
-				}
+                if (data.chargeAppliesTo.value === "Loan") {
+                    scope.chargeTimeTypeOptions = data.loanChargeTimeTypeOptions;
+                    scope.template.chargeCalculationTypeOptions = scope.template.loanChargeCalculationTypeOptions;
+                    scope.flag = false;
+                    scope.showFrequencyOptions = true;
+                } else if (data.chargeAppliesTo.value === "Savings") {
+                    scope.chargeTimeTypeOptions = data.savingsChargeTimeTypeOptions;
+                    scope.template.chargeCalculationTypeOptions = scope.template.savingsChargeCalculationTypeOptions;
+                    scope.flag = true;
+                    scope.showFrequencyOptions = false;
+                }else if(data.chargeAppliesTo.value === 'Shares') {
+                    scope.showChargePaymentByField = false;
+                    scope.chargeCalculationTypeOptions = scope.template.shareChargeCalculationTypeOptions;
+                    scope.chargeTimeTypeOptions = scope.template.shareChargeTimeTypeOptions;
+                    scope.addfeefrequency = false;
+                    scope.showGLAccount = false;
+                    scope.showPenalty = false ;
+                    scope.flag = true;
+                }else {
+                    scope.flag = true;
+                    scope.template.chargeCalculationTypeOptions = data.clientChargeCalculationTypeOptions;
+                    scope.chargeTimeTypeOptions = scope.template.clientChargeTimeTypeOptions;
+                    scope.showFrequencyOptions = false;
+                    scope.showGLAccount = true;
+                }
 
-				scope.formData = {
-					name: data.name,
-					active: data.active,
-					penalty: data.penalty,
-					currencyCode: data.currency.code,
-					chargeAppliesTo: data.chargeAppliesTo.id,
-					chargeTimeType: data.chargeTimeType.id,
-					chargeCalculationType: data.chargeCalculationType.id,
-					amount: data.amount,
-					limits: data.limits
-				};
+                scope.formData = {
+                    name: data.name,
+                    active: data.active,
+                    penalty: data.penalty,
+                    currencyCode: data.currency.code,
+                    chargeAppliesTo: data.chargeAppliesTo.id,
+                    chargeTimeType: data.chargeTimeType.id,
+                    chargeCalculationType: data.chargeCalculationType.id,
+                    amount: data.amount
+                };
 
-				scope.chargeType = data.limits && data.limits.length ? 'commission' : 'normal';
+                if(data.incomeOrLiabilityAccount){
+                    scope.formData.incomeAccountId = data.incomeOrLiabilityAccount.id;
+                }
 
-				if (data.incomeOrLiabilityAccount) {
-					scope.formData.incomeAccountId = data.incomeOrLiabilityAccount.id;
-				}
+                if(data.taxGroup){
+                    scope.formData.taxGroupId = data.taxGroup.id;
+                }
 
-				if (data.taxGroup) {
-					scope.formData.taxGroupId = data.taxGroup.id;
-				}
+                if(data.feeFrequency){
+                    scope.addfeefrequency = 'true';
+                    scope.formData.feeFrequency = data.feeFrequency.id;
+                    scope.formData.feeInterval = data.feeInterval;
+                }
 
-				if (data.feeFrequency) {
-					scope.addfeefrequency = 'true';
-					scope.formData.feeFrequency = data.feeFrequency.id;
-					scope.formData.feeInterval = data.feeInterval;
-				}
-
-				//when chargeAppliesTo is savings, below logic is
-				//to display 'Due date' field, if chargeTimeType is
-				// 'annual fee' or 'monthly fee'
-				if (scope.formData.chargeAppliesTo === 2) {
-					if (data.chargeTimeType.value === "Annual Fee" || data.chargeTimeType.value === "Monthly Fee") {
-						scope.showdatefield = true;
-						if (data.feeOnMonthDay) {
-							data.feeOnMonthDay.push(2013);
-							var actDate = dateFilter(data.feeOnMonthDay, 'dd MMMM');
-							scope.first.date = new Date(actDate);
-							//to display "Repeats Every" field ,if chargeTimeType is
-							// 'monthly fee'
-							if (data.chargeTimeType.value === "Monthly Fee") {
-								scope.repeatEvery = true;
-								scope.formData.feeInterval = data.feeInterval;
-							} else {
-								scope.repeatEvery = false;
-							}
-						}
-					} else {
-						scope.showdatefield = false;
-					}
-				} else {
-					scope.formData.chargePaymentMode = data.chargePaymentMode.id;
-				}
-			});
-			//when chargeAppliesTo is savings, below logic is
-			//to display 'Due date' field, if chargeTimeType is
-			// 'annual fee' or 'monthly fee'
-			scope.chargeTimeChange = function (chargeTimeType) {
-				if ((chargeTimeType === 12) && (scope.template.chargeAppliesTo.value === "Loan")) {
-					scope.showFrequencyOptions = false;
-				}
-				else {
-					scope.showFrequencyOptions = true;
-				}
-				if (scope.formData.chargeAppliesTo === 2) {
-					for (var i in scope.template.chargeTimeTypeOptions) {
-						if (chargeTimeType === scope.template.chargeTimeTypeOptions[i].id) {
-							if (scope.template.chargeTimeTypeOptions[i].value == "Annual Fee" || scope.template.chargeTimeTypeOptions[i].value == "Monthly Fee") {
-								scope.showdatefield = true;
-								//to show 'repeats every' field for monthly fee
-								if (scope.template.chargeTimeTypeOptions[i].value == "Monthly Fee") {
-									scope.repeatEvery = true;
-								} else {
-									scope.repeatEvery = false;
-								}
-							} else {
-								scope.showdatefield = false;
-							}
-						}
-					}
-				}
-			};
-
-			scope.filterChargeCalculations = function (chargeTimeType) {
-				return function (item) {
-					if (chargeTimeType == 12 && ((item.id == 3) || (item.id == 4))) {
-						return false;
-					}
-					if (chargeTimeType != 12 && item.id == 5) {
-						return false;
-					}
-					return true;
-				};
-			};
-
-			scope.changeChargeType = function() {
-				if (this.chargeType == 'normal') {
-					this.formData.limits = [];
-					this.stopComputingAfterDays = undefined;
-				} else if (!this.formData.limits) {
-					this.formData.limits = [];
-				}
-			};
-			scope.addLimit = function() {
-				if (this.minAmount && this.maxAmount && this.limitAmount && (parseInt(this.maxAmount) > -1 && parseFloat(this.maxAmount) >= parseFloat(this.minAmount))) {
-					this.formData.limits.push({
-						min: this.minAmount,
-						max: this.maxAmount,
-						amount: this.limitAmount,
-						chargeCalculation: this.chargeCalculation,
-						locale: scope.optlang.code
-					});
-					this.minAmount = this.maxAmount = this.limitAmount = this.chargeCalculation = undefined;
-				}
-			};
-			scope.deleteLimit = function(index) {
-				this.formData.limits.splice(index, 1);
-			};
-
-			scope.submit = function () {
-				if (scope.formData.chargeAppliesTo === 2) {
-					if (scope.showdatefield === true) {
-						var reqDate = dateFilter(scope.first.date, 'dd MMMM');
-						this.formData.monthDayFormat = 'dd MMM';
-						this.formData.feeOnMonthDay = reqDate;
-					}
-				} else if (scope.addfeefrequency == 'false') {
-					scope.formData.feeFrequency = null;
-					scope.formData.feeInterval = null;
-				}
-				if (this.formData.limits) {
-					this.formData.limits.map(function (x) { x.locale = scope.optlang.code; });
-				}
-				this.formData.locale = scope.optlang.code;
-				this.formData.active = this.formData.active || false;
-				this.formData.penalty = this.formData.penalty || false;
-				resourceFactory.chargeResource.update({chargeId: routeParams.id}, this.formData, function (data) {
-					location.path('/viewcharge/' + data.resourceId);
-				});
-			};
+                //when chargeAppliesTo is savings, below logic is
+                //to display 'Due date' field, if chargeTimeType is
+                // 'annual fee' or 'monthly fee'
+                if (scope.formData.chargeAppliesTo === 2) {
+                    if (data.chargeTimeType.value === "Annual Fee" || data.chargeTimeType.value === "Monthly Fee") {
+                        scope.showdatefield = true;
+                        if (data.feeOnMonthDay) {
+                            data.feeOnMonthDay.push(2013);
+                            var actDate = dateFilter(data.feeOnMonthDay, 'dd MMMM');
+                            scope.first.date = new Date(actDate);
+                            //to display "Repeats Every" field ,if chargeTimeType is
+                            // 'monthly fee'
+                            if (data.chargeTimeType.value === "Monthly Fee") {
+                                scope.repeatEvery = true;
+                                scope.formData.feeInterval = data.feeInterval;
+                            } else {
+                                scope.repeatEvery = false;
+                            }
+                        }
+                    } else {
+                        scope.showdatefield = false;
+                    }
+                } else {
+                    scope.formData.chargePaymentMode = data.chargePaymentMode.id;
+                }
+            });
+            //when chargeAppliesTo is savings, below logic is
+            //to display 'Due date' field, if chargeTimeType is
+            // 'annual fee' or 'monthly fee'
+            scope.chargeTimeChange = function (chargeTimeType) {
+		if ((chargeTimeType === 12) && (scope.template.chargeAppliesTo.value === "Loan"))
+		{
+			scope.showFrequencyOptions = false;
 		}
-	});
-	mifosX.ng.application.controller('EditChargeController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', mifosX.controllers.EditChargeController]).run(function ($log) {
-		$log.info("EditChargeController initialized");
-	});
+		else
+		{
+			scope.showFrequencyOptions = true;
+		}
+                if (scope.formData.chargeAppliesTo === 2) {
+                    for (var i in scope.template.chargeTimeTypeOptions) {
+                        if (chargeTimeType === scope.template.chargeTimeTypeOptions[i].id) {
+                            if (scope.template.chargeTimeTypeOptions[i].value == "Annual Fee" || scope.template.chargeTimeTypeOptions[i].value == "Monthly Fee") {
+                                scope.showdatefield = true;
+                                //to show 'repeats every' field for monthly fee
+                                if (scope.template.chargeTimeTypeOptions[i].value == "Monthly Fee") {
+                                    scope.repeatEvery = true;
+                                } else {
+                                    scope.repeatEvery = false;
+                                }
+                            } else {
+                                scope.showdatefield = false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            scope.filterChargeCalculations = function(chargeTimeType) {
+                return function (item) {
+                    if (chargeTimeType == 12 && ((item.id == 3) || (item.id == 4)))
+                    {
+                        return false;
+                    }
+                    if (chargeTimeType != 12 && item.id == 5)
+                    {
+                        return false;
+                    }
+                    return true;
+                };
+            };
+
+            scope.submit = function () {
+                if (scope.formData.chargeAppliesTo === 2) {
+                    if (scope.showdatefield === true) {
+                        var reqDate = dateFilter(scope.first.date, 'dd MMMM');
+                        this.formData.monthDayFormat = 'dd MMM';
+                        this.formData.feeOnMonthDay = reqDate;
+                    }
+                }else if(scope.addfeefrequency == 'false'){
+                    scope.formData.feeFrequency = null;
+                    scope.formData.feeInterval = null;
+                }
+                this.formData.locale = scope.optlang.code;
+                this.formData.active = this.formData.active || false;
+                this.formData.penalty = this.formData.penalty || false;
+                resourceFactory.chargeResource.update({chargeId: routeParams.id}, this.formData, function (data) {
+                    location.path('/viewcharge/' + data.resourceId);
+                });
+            };
+        }
+    });
+    mifosX.ng.application.controller('EditChargeController', ['$scope', 'ResourceFactory', '$location', '$routeParams', 'dateFilter', mifosX.controllers.EditChargeController]).run(function ($log) {
+        $log.info("EditChargeController initialized");
+    });
 }(mifosX.controllers || {}));
