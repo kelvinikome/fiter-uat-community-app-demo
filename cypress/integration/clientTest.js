c2a7ed3ee39eb06a9b488007fae7d8c0e6e4054e
Kelvin Ikome <kvnikome@gmail.com>
2019-12-10 08:12:29 +0100

    loan test: create, approve, disburse

--- cypress/integration/clientTest.js
+++ cypress/integration/clientTest.js
@@ -5,12 +5,12 @@ describe('Client Test', function () {
 
   before(() => {
     cy.viewport(1440, 1200)
-    cy.wait(5000)
     firstname = randomString(5)
     lastname = randomString(5)
   })
 
   beforeEach(() => {
+    cy.wait(4000)
     cy.login()
   })
 
@@ -18,6 +18,7 @@ describe('Client Test', function () {
     cy.visit(Cypress.env('baseUrl') + 'createclient')
     cy.get('#firstname').type(firstname)
     cy.get('#lastname').type(lastname)
+    cy.get('#activeCheckbox').click()
     cy.get('#save').click()
   })
 
@@ -36,8 +37,36 @@ describe('Client Test', function () {
     cy.get('#saveClientNote').click()
   })
 
+  it('Create and Disburse Loan', function () {
+    cy.selectClient(firstname, lastname)
+    cy.get('#generalTab').click()
+    cy.get('a[href*="#/newclientloanaccount"]').click()
+    cy.get('#productId').select('American_Loan_12M')
+
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, '0');
+    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+    var yyyy = today.getFullYear();
+
+    today = mm + '/' + dd + '/' + yyyy;
+    cy.get('#expectedDisbursementDate').type(today)
+    cy.get('#submit').click()
+    cy.get('#submitTerms').click()
+    cy.get('#submitCharges').click()
+    cy.get('#save').click()
+
+    cy.get('#loanActions').contains('Approve').click()
+    cy.get('#save').click()
+
+    cy.get('#loanActions').contains('Disburse').click()
+    cy.wait(2000)
+    cy.get('#save').click()
+
+  })
+
   Cypress.Commands.add('login', () => {
     cy.visit(Cypress.env('baseUrl'))
+    cy.wait(3000)
     cy.get('#uid').type(Cypress.env('username'))
     cy.get('#pwd').type(Cypress.env('password'))
     cy.get('#login-button').click()
@@ -49,14 +78,19 @@ describe('Client Test', function () {
     cy.get('#clientSearch').click()
     cy.get('tbody tr:first').click()
   });
+
+  Cypress.Commands.add('selectLoanAccount', (firstName, lastName) => {
+    cy.selectClient(firstname, lastName)
+    cy.get('tbody tr:first').click()
+  });
 })
 
 function randomString(string_length) {
   let random_string = '';
   let random_ascii;
-  for(let i = 0; i < string_length; i++) {
-      random_ascii = Math.floor((Math.random() * 25) + 97);
-      random_string += String.fromCharCode(random_ascii)
+  for (let i = 0; i < string_length; i++) {
+    random_ascii = Math.floor((Math.random() * 25) + 97);
+    random_string += String.fromCharCode(random_ascii)
   }
   return random_string
 }
